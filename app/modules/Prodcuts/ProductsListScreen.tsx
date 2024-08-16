17f47e9c9c87d0b6474edc91f513933f90c535ef
steveen ordoñez <zh311505@gmail.com>
2024-08-16 09:38:57 -0500

    feat: create screen product list

---
+++ app/modules/Prodcuts/ProductsListScreen.tsx
@@ -0,0 +1,14 @@
+import PageWrapper from "@/app/components/common/pages/PageWrapper";
+import { View, Text } from "react-native"
+
+const ProductsListScreen = () => {
+    return (
+        <PageWrapper>
+            <View>
+                <Text>hola</Text>
+            </View>
+        </PageWrapper>
+    )
+}
+
+export default ProductsListScreen;
