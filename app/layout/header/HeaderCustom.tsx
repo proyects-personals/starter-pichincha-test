83728f89c3b9e1b9270fa6d1f8b35587e7df2c82
steveen ordoñez <zh311505@gmail.com>
2024-08-16 09:38:42 -0500

    feat: create header custom

---
+++ app/layout/header/HeaderCustom.tsx
@@ -0,0 +1,42 @@
+import React from "react";
+import { View, Text, StyleSheet, StyleProp, ViewStyle } from "react-native";
+
+const styles = StyleSheet.create({
+  headerContainer: {
+    flexDirection: "row",
+    alignItems: "center",
+    justifyContent: "space-between",
+    paddingHorizontal: 16,
+    height: 60,
+    backgroundColor: "white",
+  },
+  icon: {
+    padding: 10,
+  },
+  headerText: {
+    fontSize: 14,
+    color: "#314573",
+    fontWeight: "bold",
+    textAlign: "center",
+    flex: 1,
+  },
+  iconPlaceholder: {
+    width: 24,
+  },
+});
+
+interface CustomHeaderProps {
+    title: string;
+    style?: StyleProp<ViewStyle>;
+  }
+
+  const HeaderCustom: React.FC<CustomHeaderProps> = ({ title, style }) => {
+    return (
+      <View style={styles.headerContainer}>
+        <Text style={styles.headerText}>{title}</Text>
+        <View style={styles.iconPlaceholder} />
+      </View>
+    );
+  };
+
+export default HeaderCustom;
